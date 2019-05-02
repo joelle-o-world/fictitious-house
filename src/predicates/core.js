054ebb2da670aede9b1bdda3b79a00b7187d0851
Joel Plowright <joelplow@gmail.com>
2019-05-02 18:16:30 +0100

    reorganised predicates

---
+++ src/predicates/core.js
@@ -0,0 +1,68 @@
+const {Predicate, sub} = require('english-io')
+const TimedPredicate = require('../TimedPredicate')
+const adjectives = require('../adjectives')
+
+module.exports = {
+  // LOCATION
+
+  // OTHER
+  beCalled: new Predicate({
+    verb:'be',
+    params:['subject', '@called'],
+    begin: (A, name) => A.name = name,
+    check: (A, name) => A.name == name,
+  }),
+
+  be: new Predicate({
+    verb: 'be',
+    params: ['subject', '@object'],
+    problem(entity, adjective) {
+      if(!adjectives[adjective])
+        return sub('there is no such adjective as "_"', adjective)
+    },
+    begin: (entity, adjective) => entity.be(adjective),
+  }),
+
+  beA: new Predicate({
+    verb: 'be',
+    params: ['subject', '@a'],
+    begin: (entity, classname) => entity.be_a(classname),
+    check: (entity, className) => entity.is_a(className),
+    banal: true,
+  }),
+
+  smoke: new Predicate({
+    verb: 'smoke',
+    params: ['subject', 'object'],
+  }),
+
+  love: new Predicate({
+    verb: 'love',
+    params: ['subject', 'object'],
+  }),
+
+  jump: new TimedPredicate({
+    verb: 'jump', params:['subject'],
+    duration:5,
+  }),
+
+  steal: new Predicate({
+    verb:'steal',
+    params: ['subject', 'object'],
+    until: callback => callback(),
+  }),
+
+  thereIs: new Predicate({
+    forms: [
+      {verb:'be', params:['object'], constants:{_subject:'there'}},
+      {verb:'exist', params:['subject']},
+    ],
+    banal: true,
+  }),
+
+  gaveTo: new Predicate({
+    forms: [
+      {verb:'give', params:['subject', 'object', 'to']},
+    ]
+  })
+}
--- src/predicates/index.js
+++ src/predicates/index.js
@@ -1,70 +1,8 @@
-const {Predicate} = require('english-io')
-const TimedPredicate = require('../TimedPredicate')
-
-module.exports = {
-  // LOCATION
-
-  // OTHER
-  beCalled: new Predicate({
-    verb:'be',
-    params:['subject', '@called'],
-    begin: (A, name) => A.name = name,
-    check: (A, name) => A.name == name,
-  }),
-
-  be: new Predicate({
-    verb: 'be',
-    params: ['subject', '@object'],
-    begin: (entity, adjective) => entity.be(adjective),
-  }),
-
-  beA: new Predicate({
-    verb: 'be',
-    params: ['subject', '@a'],
-    begin: (entity, classname) => entity.be_a(classname),
-    check: (entity, className) => entity.is_a(className),
-    banal: true,
-  }),
-
-  smoke: new Predicate({
-    verb: 'smoke',
-    params: ['subject', 'object'],
-  }),
-
-  love: new Predicate({
-    verb: 'love',
-    params: ['subject', 'object'],
-  }),
-
-  jump: new TimedPredicate({
-    verb: 'jump', params:['subject'],
-    duration:5,
-  }),
-
-  steal: new Predicate({
-    verb:'steal',
-    params: ['subject', 'object'],
-    until: callback => callback(),
-  }),
-
-  thereIs: new Predicate({
-    forms: [
-      {verb:'be', params:['object'], constants:{_subject:'there'}},
-      {verb:'exist', params:['subject']},
-    ],
-    banal: true,
-  }),
-
-  gaveTo: new Predicate({
-    forms: [
-      {verb:'give', params:['subject', 'object', 'to']},
-    ]
-  })
-}
-
+Object.assign(module.exports, require('./core'))
 Object.assign(module.exports, require('./location'))
 Object.assign(module.exports, require('./movement'))
 Object.assign(module.exports, require('./actions'))
 Object.assign(module.exports, require('./sound'))
 Object.assign(module.exports, require('./fashion'))
+
 module.exports.goTo = require('./goTo')
