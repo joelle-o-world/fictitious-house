7748e6566f8486040eae3e1eaa6451b7efd1cacc
Joel Plowright <joelplow@gmail.com>
2019-05-07 16:28:43 +0100

    index-folie

---
+++ util/index-folie.js
@@ -0,0 +1,24 @@
+/*
+  Produce an index of the folie directory. Plop it in folie/index.js.
+*/
+
+const { lstatSync, readdirSync, writeFileSync } = require('fs')
+const { join, basename } = require('path')
+const findFiles = require('./findFiles')
+
+const isDirectory = source => lstatSync(source).isDirectory()
+const getDirectories = source =>
+  readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+
+const dirs = getDirectories('./folie')
+
+
+let index = {}
+for(let dir of dirs) {
+  let files = findFiles(dir, ['.mp3', '.wav'])
+  index[basename(dir)] = files
+}
+
+
+writeFileSync('./folie/index.json', JSON.stringify(index))
+console.log('done')
