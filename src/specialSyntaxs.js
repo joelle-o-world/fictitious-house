b350a7fff310a56876c94bc0ec663b6d708b9a0a
Joel Plowright <joelplow@gmail.com>
2019-05-16 17:32:42 +0100

    Special Syntaxs for scheduling

--- src/specialSyntaxs.js
+++ src/specialSyntaxs.js
@@ -1,6 +1,6 @@
 const {SpecialSentenceSyntax} = require('english-io')
 
-const Every = new SpecialSentenceSyntax('every #_ seconds, ~_', {
+const EverySeconds_ = new SpecialSentenceSyntax('every #_ seconds,? ~_', {
   start([d, sentence], domain) {
     setInterval(() => {
       sentence.start(domain)
@@ -8,4 +8,31 @@ const Every = new SpecialSentenceSyntax('every #_ seconds, ~_', {
   }
 })
 
-module.exports.Every = Every
+const _EverySeconds = new SpecialSentenceSyntax('~_,? every #_ seconds', {
+  start([sentence, d], domain) {
+    setInterval(() => {
+      sentence.start(domain)
+    }, d*1000)
+  }
+})
+
+const _After_Seconds = new SpecialSentenceSyntax('~_,? after #_ seconds', {
+  start([sentence, d], domain) {
+    setTimeout(() => {
+      sentence.start(domain)
+    }, d*1000)
+  }
+})
+
+const After_Seconds_ = new SpecialSentenceSyntax('after #_ seconds,? ~_', {
+  start([d, sentence], domain) {
+    setTimeout(() => {
+      sentence.start(domain)
+    }, d*1000)
+  }
+})
+
+module.exports.EverySeconds_ = EverySeconds_
+module.exports._EverySeconds = _EverySeconds
+module.exports._After_Seconds = _After_Seconds
+module.exports.After_Seconds_ = After_Seconds_
